dd3a2fb02ff47a93592ae499eef05ee802a0016e
koki murakami <koki.m3626@gmail.com>
2020-06-05 14:46:07 +0000

    プロフィール

--- edit.blade.php
+++ edit.blade.php
@@ -1,13 +1,52 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.profile')
+@section('title', 'プロフィールの編集')
 
-        <title>Edit</title>
-    </head>
-    <body>
-        <h1>Edit作成画面</h1>
-    </body>
-</html>
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール編集</h2>
+                <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
+                    @if (count($errors) > 0)
+                    <ul>
+                        @foreach($errors->all() as $e)
+                        <li>{{ $e }}</li>
+                        @endforeach
+                    </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ $profile_form->name }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <input type="radio" id="man" name="gender" value='man' {{ $profile_form->gender == 'man' ? 'man' :''}}>
+                            <label for="man">男</label>
+                            <input type="radio" id="woman" name="gender" value='woman' {{ $profile_form->gender == 'woman' ? 'woman' :''}}>
+                            <label for="woman">女</label>
+                            <input type="radio" id="other" name="gender" value='other' {{ $profile_form->gender == 'other' ? 'other' :''}}>
+                            <label for="other">その他</label>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ $profile_form->hobby }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ $profile_form->introduction }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
